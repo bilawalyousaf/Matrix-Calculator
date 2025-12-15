a6cfd4bf77c389dd1bf8ee18f39490af85d42622
Bilawal <bilawalkhan57777@gmail.com>
2025-12-15 13:38:04 +0500

    Iam Made Matrix Calculator For Students

---
+++ 4x4.js
@@ -0,0 +1,286 @@
+function add_matrix() {
+
+    let arr1 = []
+    let arr2 = []
+
+    arr1[0] = document.getElementById("matrix-1").value
+    arr1[1] = document.getElementById("matrix-2").value
+    arr1[2] = document.getElementById("matrix-3").value
+    arr1[3] = document.getElementById("matrix-4").value
+    arr1[4] = document.getElementById("matrix-5").value
+    arr1[5] = document.getElementById("matrix-6").value
+    arr1[6] = document.getElementById("matrix-7").value
+    arr1[7] = document.getElementById("matrix-8").value
+    arr1[8] = document.getElementById("matrix-9").value
+    arr1[9] = document.getElementById("matrix-10").value
+    arr1[10] = document.getElementById("matrix-11").value
+    arr1[11] = document.getElementById("matrix-12").value
+    arr1[12] = document.getElementById("matrix-13").value
+    arr1[13] = document.getElementById("matrix-14").value
+    arr1[14] = document.getElementById("matrix-15").value
+    arr1[15] = document.getElementById("matrix-16").value
+
+    arr2[0] = document.getElementById("matrix-17").value
+    arr2[1] = document.getElementById("matrix-18").value
+    arr2[2] = document.getElementById("matrix-19").value
+    arr2[3] = document.getElementById("matrix-20").value
+    arr2[4] = document.getElementById("matrix-21").value
+    arr2[5] = document.getElementById("matrix-22").value
+    arr2[6] = document.getElementById("matrix-23").value
+    arr2[7] = document.getElementById("matrix-24").value
+    arr2[8] = document.getElementById("matrix-25").value
+    arr2[9] = document.getElementById("matrix-26").value
+    arr2[10] = document.getElementById("matrix-27").value
+    arr2[11] = document.getElementById("matrix-28").value
+    arr2[12] = document.getElementById("matrix-29").value
+    arr2[13] = document.getElementById("matrix-30").value
+    arr2[14] = document.getElementById("matrix-31").value
+    arr2[15] = document.getElementById("matrix-32").value
+
+    let result = []
+    result[0] = Number(arr1[0]) + Number(arr2[0])
+    result[1] = Number(arr1[1]) + Number(arr2[1])
+    result[2] = Number(arr1[2]) + Number(arr2[2])
+    result[3] = Number(arr1[3]) + Number(arr2[3])
+    result[4] = Number(arr1[4]) + Number(arr2[4])
+    result[5] = Number(arr1[5]) + Number(arr2[5])
+    result[6] = Number(arr1[6]) + Number(arr2[6])
+    result[7] = Number(arr1[7]) + Number(arr2[7])
+    result[8] = Number(arr1[8]) + Number(arr2[8])
+    result[9] = Number(arr1[9]) + Number(arr2[9])
+    result[10] = Number(arr1[10]) + Number(arr2[10])
+    result[11] = Number(arr1[11]) + Number(arr2[11])
+    result[12] = Number(arr1[12]) + Number(arr2[12])
+    result[13] = Number(arr1[13]) + Number(arr2[13])
+    result[14] = Number(arr1[14]) + Number(arr2[14])
+    result[15] = Number(arr1[15]) + Number(arr2[15])
+
+
+    document.getElementById("matrix-33").value = result[0]
+    document.getElementById("matrix-34").value = result[1]
+    document.getElementById("matrix-35").value = result[2]
+    document.getElementById("matrix-36").value = result[3]
+    document.getElementById("matrix-37").value = result[4]
+    document.getElementById("matrix-38").value = result[5]
+    document.getElementById("matrix-39").value = result[6]
+    document.getElementById("matrix-40").value = result[7]
+    document.getElementById("matrix-41").value = result[8]
+    document.getElementById("matrix-42").value = result[9]
+    document.getElementById("matrix-43").value = result[10]
+    document.getElementById("matrix-44").value = result[11]
+    document.getElementById("matrix-45").value = result[12]
+    document.getElementById("matrix-46").value = result[13]
+    document.getElementById("matrix-47").value = result[14]
+    document.getElementById("matrix-48").value = result[15]
+
+}
+
+function subtract_matrix() {
+
+    let arr1 = []
+    let arr2 = []
+
+    arr1[0] = document.getElementById("matrix-1").value
+    arr1[1] = document.getElementById("matrix-2").value
+    arr1[2] = document.getElementById("matrix-3").value
+    arr1[3] = document.getElementById("matrix-4").value
+    arr1[4] = document.getElementById("matrix-5").value
+    arr1[5] = document.getElementById("matrix-6").value
+    arr1[6] = document.getElementById("matrix-7").value
+    arr1[7] = document.getElementById("matrix-8").value
+    arr1[8] = document.getElementById("matrix-9").value
+    arr1[9] = document.getElementById("matrix-10").value
+    arr1[10] = document.getElementById("matrix-11").value
+    arr1[11] = document.getElementById("matrix-12").value
+    arr1[12] = document.getElementById("matrix-13").value
+    arr1[13] = document.getElementById("matrix-14").value
+    arr1[14] = document.getElementById("matrix-15").value
+    arr1[15] = document.getElementById("matrix-16").value
+
+    arr2[0] = document.getElementById("matrix-17").value
+    arr2[1] = document.getElementById("matrix-18").value
+    arr2[2] = document.getElementById("matrix-19").value
+    arr2[3] = document.getElementById("matrix-20").value
+    arr2[4] = document.getElementById("matrix-21").value
+    arr2[5] = document.getElementById("matrix-22").value
+    arr2[6] = document.getElementById("matrix-23").value
+    arr2[7] = document.getElementById("matrix-24").value
+    arr2[8] = document.getElementById("matrix-25").value
+    arr2[9] = document.getElementById("matrix-26").value
+    arr2[10] = document.getElementById("matrix-27").value
+    arr2[11] = document.getElementById("matrix-28").value
+    arr2[12] = document.getElementById("matrix-29").value
+    arr2[13] = document.getElementById("matrix-30").value
+    arr2[14] = document.getElementById("matrix-31").value
+    arr2[15] = document.getElementById("matrix-32").value
+
+    let result = []
+    result[0] = Number(arr1[0]) - Number(arr2[0])
+    result[1] = Number(arr1[1]) - Number(arr2[1])
+    result[2] = Number(arr1[2]) - Number(arr2[2])
+    result[3] = Number(arr1[3]) - Number(arr2[3])
+    result[4] = Number(arr1[4]) - Number(arr2[4])
+    result[5] = Number(arr1[5]) - Number(arr2[5])
+    result[6] = Number(arr1[6]) - Number(arr2[6])
+    result[7] = Number(arr1[7]) - Number(arr2[7])
+    result[8] = Number(arr1[8]) - Number(arr2[8])
+    result[9] = Number(arr1[9]) - Number(arr2[9])
+    result[10] = Number(arr1[10]) - Number(arr2[10])
+    result[11] = Number(arr1[11]) - Number(arr2[11])
+    result[12] = Number(arr1[12]) - Number(arr2[12])
+    result[13] = Number(arr1[13]) - Number(arr2[13])
+    result[14] = Number(arr1[14]) - Number(arr2[14])
+    result[15] = Number(arr1[15]) - Number(arr2[15])
+
+
+    document.getElementById("matrix-33").value = result[0]
+    document.getElementById("matrix-34").value = result[1]
+    document.getElementById("matrix-35").value = result[2]
+    document.getElementById("matrix-36").value = result[3]
+    document.getElementById("matrix-37").value = result[4]
+    document.getElementById("matrix-38").value = result[5]
+    document.getElementById("matrix-39").value = result[6]
+    document.getElementById("matrix-40").value = result[7]
+    document.getElementById("matrix-41").value = result[8]
+    document.getElementById("matrix-42").value = result[9]
+    document.getElementById("matrix-43").value = result[10]
+    document.getElementById("matrix-44").value = result[11]
+    document.getElementById("matrix-45").value = result[12]
+    document.getElementById("matrix-46").value = result[13]
+    document.getElementById("matrix-47").value = result[14]
+    document.getElementById("matrix-48").value = result[15]
+
+}
+
+function multiply_matrix() {
+
+    let arr1 = []
+    let arr2 = []
+
+    arr1[0] = document.getElementById("matrix-1").value
+    arr1[1] = document.getElementById("matrix-2").value
+    arr1[2] = document.getElementById("matrix-3").value
+    arr1[3] = document.getElementById("matrix-4").value
+    arr1[4] = document.getElementById("matrix-5").value
+    arr1[5] = document.getElementById("matrix-6").value
+    arr1[6] = document.getElementById("matrix-7").value
+    arr1[7] = document.getElementById("matrix-8").value
+    arr1[8] = document.getElementById("matrix-9").value
+    arr1[9] = document.getElementById("matrix-10").value
+    arr1[10] = document.getElementById("matrix-11").value
+    arr1[11] = document.getElementById("matrix-12").value
+    arr1[12] = document.getElementById("matrix-13").value
+    arr1[13] = document.getElementById("matrix-14").value
+    arr1[14] = document.getElementById("matrix-15").value
+    arr1[15] = document.getElementById("matrix-16").value
+
+    arr2[0] = document.getElementById("matrix-17").value
+    arr2[1] = document.getElementById("matrix-18").value
+    arr2[2] = document.getElementById("matrix-19").value
+    arr2[3] = document.getElementById("matrix-20").value
+    arr2[4] = document.getElementById("matrix-21").value
+    arr2[5] = document.getElementById("matrix-22").value
+    arr2[6] = document.getElementById("matrix-23").value
+    arr2[7] = document.getElementById("matrix-24").value
+    arr2[8] = document.getElementById("matrix-25").value
+    arr2[9] = document.getElementById("matrix-26").value
+    arr2[10] = document.getElementById("matrix-27").value
+    arr2[11] = document.getElementById("matrix-28").value
+    arr2[12] = document.getElementById("matrix-29").value
+    arr2[13] = document.getElementById("matrix-30").value
+    arr2[14] = document.getElementById("matrix-31").value
+    arr2[15] = document.getElementById("matrix-32").value
+
+    let result = []
+    result[0] = Number(arr1[0]) * Number(arr2[0])
+    result[1] = Number(arr1[1]) * Number(arr2[1])
+    result[2] = Number(arr1[2]) * Number(arr2[2])
+    result[3] = Number(arr1[3]) * Number(arr2[3])
+    result[4] = Number(arr1[4]) * Number(arr2[4])
+    result[5] = Number(arr1[5]) * Number(arr2[5])
+    result[6] = Number(arr1[6]) * Number(arr2[6])
+    result[7] = Number(arr1[7]) * Number(arr2[7])
+    result[8] = Number(arr1[8]) * Number(arr2[8])
+    result[9] = Number(arr1[9]) * Number(arr2[9])
+    result[10] = Number(arr1[10]) * Number(arr2[10])
+    result[11] = Number(arr1[11]) * Number(arr2[11])
+    result[12] = Number(arr1[12]) * Number(arr2[12])
+    result[13] = Number(arr1[13]) * Number(arr2[13])
+    result[14] = Number(arr1[14]) * Number(arr2[14])
+    result[15] = Number(arr1[15]) * Number(arr2[15])
+
+
+    document.getElementById("matrix-33").value = result[0]
+    document.getElementById("matrix-34").value = result[1]
+    document.getElementById("matrix-35").value = result[2]
+    document.getElementById("matrix-36").value = result[3]
+    document.getElementById("matrix-37").value = result[4]
+    document.getElementById("matrix-38").value = result[5]
+    document.getElementById("matrix-39").value = result[6]
+    document.getElementById("matrix-40").value = result[7]
+    document.getElementById("matrix-41").value = result[8]
+    document.getElementById("matrix-42").value = result[9]
+    document.getElementById("matrix-43").value = result[10]
+    document.getElementById("matrix-44").value = result[11]
+    document.getElementById("matrix-45").value = result[12]
+    document.getElementById("matrix-46").value = result[13]
+    document.getElementById("matrix-47").value = result[14]
+    document.getElementById("matrix-48").value = result[15]
+
+}
+
+function resetAll() {
+
+    document.getElementById("matrix-1").value = "1";
+    document.getElementById("matrix-2").value = "2";
+    document.getElementById("matrix-3").value = "3";
+    document.getElementById("matrix-4").value = "4";
+    document.getElementById("matrix-5").value = "5";
+    document.getElementById("matrix-6").value = "6";
+    document.getElementById("matrix-7").value = "7";
+    document.getElementById("matrix-8").value = "8";
+    document.getElementById("matrix-9").value = "9";
+    document.getElementById("matrix-10").value = "10";
+    document.getElementById("matrix-11").value = "11";
+    document.getElementById("matrix-12").value = "12";
+    document.getElementById("matrix-13").value = "13";
+    document.getElementById("matrix-14").value = "14";
+    document.getElementById("matrix-15").value = "15";
+    document.getElementById("matrix-16").value = "16";
+
+    document.getElementById("matrix-17").value = "1";
+    document.getElementById("matrix-18").value = "2";
+    document.getElementById("matrix-19").value = "3";
+    document.getElementById("matrix-20").value = "4";
+    document.getElementById("matrix-21").value = "5";
+    document.getElementById("matrix-22").value = "6";
+    document.getElementById("matrix-23").value = "7";
+    document.getElementById("matrix-24").value = "8";
+    document.getElementById("matrix-25").value = "9";
+    document.getElementById("matrix-26").value = "9";
+    document.getElementById("matrix-27").value = "9";
+    document.getElementById("matrix-27").value = "9";
+    document.getElementById("matrix-29").value = "9";
+    document.getElementById("matrix-30").value = "9";
+    document.getElementById("matrix-31").value = "9";
+    document.getElementById("matrix-32").value = "9";
+
+
+    document.getElementById("matrix-33").value = "";
+    document.getElementById("matrix-34").value = "";
+    document.getElementById("matrix-35").value = "";
+    document.getElementById("matrix-36").value = "";
+    document.getElementById("matrix-37").value = "";
+    document.getElementById("matrix-38").value = "";
+    document.getElementById("matrix-39").value = "";
+    document.getElementById("matrix-40").value = "";
+    document.getElementById("matrix-41").value = "";
+    document.getElementById("matrix-42").value = "";
+    document.getElementById("matrix-43").value = "";
+    document.getElementById("matrix-44").value = "";
+    document.getElementById("matrix-45").value = "";
+    document.getElementById("matrix-46").value = "";
+    document.getElementById("matrix-47").value = "";
+    document.getElementById("matrix-48").value = "";
+
+}
